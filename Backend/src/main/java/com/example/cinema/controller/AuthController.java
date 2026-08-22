2d2074bfb89506735651a4441b3a0391be284936
Vo Xuan Duong <xuanduong.261104@gmail.com>
2026-08-22 14:29:21 +0700

    feat: add registration and login endpoints to AuthController and update AuthResponse structure

--- Backend/src/main/java/com/example/cinema/controller/AuthController.java
+++ Backend/src/main/java/com/example/cinema/controller/AuthController.java
@@ -6,5 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auths")
 public class AuthController {
+    private final AuthService authService;
+
+    public AuthController (AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<AuthResponse>> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(
+                new ApiResponse<>(authService.register(request))
+        );
+    }
+
+    @PostMapping("/login")
+    public 
 
 }
--- Backend/src/main/java/com/example/cinema/dto/auth/AuthResponse.java
+++ Backend/src/main/java/com/example/cinema/dto/auth/AuthResponse.java
@@ -1,6 +1,7 @@
 package com.example.cinema.dto.auth;
 
 public record AuthResponse(
+        UUID userId,
         String accessToken,
         String refreshToken
 ) {
