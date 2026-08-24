776ed5d179196797d378cbc1d46239fe5ddb7823
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-24 13:31:11 +0700

    booking: enable scheduled maintenance tasks

--- Backend/src/main/java/com/example/cinema/CinemaApplication.java
+++ Backend/src/main/java/com/example/cinema/CinemaApplication.java
@@ -2,12 +2,13 @@ package com.example.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CinemaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinemaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApplication.class, args);
+    }
 }
