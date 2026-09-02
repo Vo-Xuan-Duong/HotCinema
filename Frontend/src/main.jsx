5fe09afb2e63afc9d9f56fdba65acb6ebdbacdce
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-02 19:21:35 +0700

    refactor(ui): remove global important style overrides

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import '@/mocks/bootstrapMockApi';
 import App from '@/App';
 import './index.css';
-import './styles/flat-ui-policy.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
