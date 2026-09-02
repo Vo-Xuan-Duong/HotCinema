620e7227f0acfae236da97c1df19ed6407a42d23
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-02 19:18:55 +0700

    refactor(ui): simplify customer layout providers

--- Frontend/src/layouts/UserLayout.jsx
+++ Frontend/src/layouts/UserLayout.jsx
@@ -1,19 +1,15 @@
-﻿import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import AuthModal from '@/components/Auth/AuthModal';
+import FloatingSupport from '@/components/FloatingSupport/FloatingSupport';
 import GlobalBackTop from '@/components/GlobalBackTop/GlobalBackTop';
-import { Outlet } from 'react-router-dom';
-import { TrailerModalProvider } from '@/context/TrailerModalContext';
 import { AuthModalProvider, useAuthModal } from '@/context/AuthModalContext';
 import { setAuthErrorCallback } from '@/utils/apiClient';
 import ScrollToTop from '@/components/ScrollToTop';
 
-// Memoized Header để tránh re-render không cần thiết
 const MemoizedHeader = React.memo(Header);
-
-// Memoized Footer để tránh re-render không cần thiết
 const MemoizedFooter = React.memo(Footer);
 
 const UserLayoutContent = () => {
@@ -27,45 +23,36 @@ const UserLayoutContent = () => {
     };
 
     setAuthErrorCallback(handleAuthError);
-
-    return () => {
-      setAuthErrorCallback(null);
-    };
+    return () => setAuthErrorCallback(null);
   }, [openAuthModal, location.pathname]);
 
   return (
-    <TrailerModalProvider>
+    <>
       <ScrollToTop />
       <div className="flex min-h-dvh flex-col bg-background text-foreground">
-        {/* Header chỉ re-render khi cần thiết */}
         <MemoizedHeader />
-        {/* Chỉ phần này sẽ re-render khi route thay đổi */}
         <main key={location.pathname} className="min-w-0 flex-1 bg-background">
           <Outlet />
         </main>
-        {/* Footer không re-render khi route thay đổi */}
         <MemoizedFooter />
 
-        {/* Global Auth Modal */}
         <AuthModal
           isOpen={isAuthModalOpen}
           onClose={closeAuthModal}
           initialMode={authModalMode}
         />
 
-        {/* Global Back to Top */}
         <GlobalBackTop visibilityHeight={200} />
+        <FloatingSupport />
       </div>
-    </TrailerModalProvider>
+    </>
   );
 };
 
-const UserLayout = () => {
-  return (
-    <AuthModalProvider>
-      <UserLayoutContent />
-    </AuthModalProvider>
-  );
-};
+const UserLayout = () => (
+  <AuthModalProvider>
+    <UserLayoutContent />
+  </AuthModalProvider>
+);
 
 export default UserLayout;
